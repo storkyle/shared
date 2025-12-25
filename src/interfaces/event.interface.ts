98f25745770d871a4b576ce551c0ce6eb65e4e05
Giang Huynh <trgianghuynhdev1808@gmail.com>
2025-12-25 10:40:02 +0700

    fix: :wrench: missed IEventPayload interface

---
+++ src/interfaces/event.interface.ts
@@ -0,0 +1,4 @@
+export interface IEventPayload<T> {
+  target: T;
+  actor: string;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './actor.interface';
 export * from './context.interface';
+export * from './event.interface';
 export * from './filter.interface';
